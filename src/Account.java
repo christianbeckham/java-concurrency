3fbcbcd96c3d30b3e8b7b053b900c61b00b96aba
Christian Beckham <urenacb22@gmail.com>
2022-11-10 12:04:30 -0600

    feat: created Account class, created getter & setter methods to access private variables, and created withdraw method

---
+++ src/Account.java
@@ -0,0 +1,34 @@
+public class Account {
+    private int balance;
+    private int amountWithdrawn;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getAmountWithdrawn() {
+        return amountWithdrawn;
+    }
+
+    public void setAmountWithdrawn(int amountWithdrawn) {
+        this.amountWithdrawn = amountWithdrawn;
+    }
+
+    public void withdraw(int amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            this.amountWithdrawn += amount;
+            System.out.println(amount + " withdrawn. New balance " + this.balance + ". Total withdrawn: " + this.amountWithdrawn);
+        } else {
+            System.out.println("Insufficient funds. Total withdrawn: " + this.amountWithdrawn);
+        }
+    }
+}
